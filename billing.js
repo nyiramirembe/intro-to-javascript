f0ff1073b8824a60116dc6eeb678400331d496fb
Nyiramirembe <peninahhot@gmail.com>
2019-06-14 08:05:14 -0700

    @

--- billing.js
+++ billing.js
@@ -1,5 +1,1 @@
-/*this is the percentage thatdetermines the transaction charge*/
-const transactionCharge = 0.025;
-var amountTendered = 100000;
-console.log("this is what the billed amount is (lesstransaction charges)");
-console.log(amountTendered-(transactionCharge*amountTendered));
+for (i=1;i<=100;i)
